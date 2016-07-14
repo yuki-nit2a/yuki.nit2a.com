5ec9bb13d566867bd84f80dda4c4b0741aea1026
yuki-nit2a <yuki@nit2a.com>
2016-07-14 11:50:28 +0900

    Add basic text&layout

--- src/app/src/View/Index.php
+++ src/app/src/View/Index.php
@@ -5,10 +5,9 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title></title>
+        <title>Yuki Nitta</title>
 
-        <meta name="description" content="">
-        <meta name="keywords" content="">
+        <meta name="description" content="profile">
 
         <meta property="icon" content="/favicon.ico">
         <link rel="icon" type="image/vnd.microsoft.icon" href="/favicon.ico">
@@ -16,31 +15,55 @@
         <link rel="stylesheet" href="/css/base.css">
 
         <!--[if lt IE 9]>
-        <script src="/js/ie.js"></script>
+        <script src="/js/polyfill/ie.js"></script>
         <![endif]-->
     </head>
 
     <body>
-        <header>
-            <h1><a href="/">Yuki Nitta</a></h1>
-        </header>
+        <div id="board">
+            <header>
+                <h1>Yuki Nitta</h1>
+            </header>
 
-        <main>
-            <h1></h1>
+            <main>
+                <h1>Profile</h1>
 
-            <section>
-                <h1></h1>
-            </section>
+                <dl>
+                    <dt>Alias</dt>
+                    <dd>yuki-nit2a</dd>
 
-            <section>
-                <h1></h1>
-            </section>
+                    <dt>Position</dt>
+                    <dd>Full stack engineer</dd>
+                    <dd>of web development.</dd>
 
-            <section>
-                <h1></h1>
-            </section>
-        </main>
+                    <dt>Interest</dt>
+                    <dd>Philosophy</dd>
+
+                    <dt>E-mail</dt>
+                    <dd>y&#117;&#107;i@&#110;&#105;&#116;2&#97;&#46;&#99;&#111;m</dd>
+
+                    <dt>Media</dt>
+                    <dd><a href="https://github.com/yuki-nit2a" target="_blank">GitHub</a></dd>
+
+                    <dt>Board</dt>
+                    <dd>I'm between jobs</dd>
+                    <dd>to make some www.</dd>
+                </dl>
+            </main>
+        </div>
+
+        <div id="background"></div>
 
         <script src="/js/base.js"></script>
+
+        <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-25136597-1', 'auto');
+        ga('send', 'pageview');
+        </script>
     </body>
 </html>
